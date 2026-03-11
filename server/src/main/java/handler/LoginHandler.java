b28f14fe690c4e71bbfbb001ffdf4eb69525d35e
mnkoster <144175990+mnkoster@users.noreply.github.com>
2026-03-10 21:40:34 -0600

    impl (p4) - updated LoginHandler to catch exceptions

--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,10 +1,13 @@
 package handler;
 
 import dataaccess.DataAccessException;
+import dataaccess.UnauthorizedException;
 import io.javalin.http.Context;
 import requests.LoginRequest;
 import results.LoginResult;
 import service.UserService;
+
+import java.util.Map;
 
 /**
  * 3/1/26: added handler for p3 apis (login)
@@ -18,15 +21,24 @@ public class LoginHandler {
     }
 
     public void handle(Context ctx) {
-        LoginRequest request = ctx.bodyAsClass(LoginRequest.class);
-        if (request.username() == null || request.password() == null) {
-            ctx.status(400).json(new ErrorResponse("Error: bad request"));
-            return;
+        try {
+            LoginRequest request = ctx.bodyAsClass(LoginRequest.class);
+
+            if (request.username() == null || request.password() == null) {
+                ctx.status(400).json(new ErrorResponse("Error: bad request"));
+                return;
+            }
+
+            LoginResult result = userService.login(
+                    request.username(),
+                    request.password()
+            );
+
+            ctx.status(200).json(result);
+        } catch (UnauthorizedException e) {
+            ctx.status(401).json(new ErrorResponse(e.getMessage()));
+        } catch (DataAccessException e) {
+            ctx.status(500).json(new ErrorResponse("Error: server error"));
         }
-        LoginResult result = userService.login(
-                request.username(),
-                request.password()
-        );
-        ctx.status(200).json(result);
     }
 }
