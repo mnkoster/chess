2a498622adba648af7ad6dd834f77dab18cee261
mnkoster <144175990+mnkoster@users.noreply.github.com>
2026-03-11 21:13:51 -0600

    impl (p4) - fixing

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,23 +1,23 @@
 package server;
 
-import io.javalin.*;
-import dataaccess.*;
+import io.javalin.Javalin;
 import io.javalin.json.JavalinGson;
+
+import dataaccess.*;
 import service.*;
 import handler.*;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-/**
- * 2/28/26: added some logic
- * 3/1/26: added register, login, clear // service, handler, exceptions
- */
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
+
         Gson gson = new GsonBuilder().serializeNulls().create();
+
         javalin = Javalin.create(config -> {
             config.staticFiles.add("web");
             config.jsonMapper(new JavalinGson(gson, false));
@@ -27,22 +27,21 @@ public class Server {
         UserDAO userDAO;
         AuthDAO authDAO;
         GameDAO gameDAO;
+
         try {
             userDAO = new SQLUserDAO();
             authDAO = new SQLAuthDAO();
             gameDAO = new SQLGameDAO();
-//        } catch (DataAccessException e) {
-//            userDAO = new MemoryUserDAO();
-//            authDAO = new MemoryAuthDAO();
-//            gameDAO = new MemoryGameDAO();
-//        }
+        } catch (DataAccessException e) {
+            throw new RuntimeException("Database failed to initialize", e);
+        }
 
-        // Service
+        // Services
         UserService userService = new UserService(userDAO, authDAO);
         ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
         GameService gameService = new GameService(gameDAO, authDAO);
 
-        // Handler
+        // Handlers
         RegisterHandler registerHandler = new RegisterHandler(userService);
         LoginHandler loginHandler = new LoginHandler(userService);
         LogoutHandler logoutHandler = new LogoutHandler(userService);
@@ -51,7 +50,7 @@ public class Server {
         JoinGameHandler joinGameHandler = new JoinGameHandler(gameService);
         ClearHandler clearHandler = new ClearHandler(clearService);
 
-        // Route
+        // Routes
         javalin.post("/user", registerHandler::handle);
         javalin.post("/session", loginHandler::handle);
         javalin.delete("/session", logoutHandler::handle);
@@ -59,22 +58,23 @@ public class Server {
         javalin.post("/game", createGameHandler::handle);
         javalin.put("/game", joinGameHandler::handle);
         javalin.delete("/db", clearHandler::handle);
-        } catch (DataAccessException e) {
-            throw new RuntimeException("Database failed", e);
-        }
+
         // Exceptions
         javalin.exception(BadRequestException.class, (e, ctx) -> {
             ctx.status(400).json(new ErrorResponse(e.getMessage()));
-        }); // 400: bad request
+        });
+
         javalin.exception(UnauthorizedException.class, (e, ctx) -> {
             ctx.status(401).json(new ErrorResponse(e.getMessage()));
-        }); // 401: unauthorized (wrong username/password)
+        });
+
         javalin.exception(AlreadyTakenException.class, (e, ctx) -> {
             ctx.status(403).json(new ErrorResponse(e.getMessage()));
-        }); // 403: username already taken
+        });
+
         javalin.exception(Exception.class, (e, ctx) -> {
-            ctx.status(500).json(new ErrorResponse(e.getMessage()));
-        }); // 500: other errors
+            ctx.status(500).json(new ErrorResponse("Error: server error"));
+        });
     }
 
     public int run(int desiredPort) {
