24d4ec2f7f16187663b3ee5290962d955d7d7ba2
mnkoster <144175990+mnkoster@users.noreply.github.com>
2026-03-11 22:06:36 -0600

    impl (p4) - SQLAuthDAOTests written and passing

---
+++ server/src/test/java/dataaccess/SQLAuthDAOTests.java
@@ -0,0 +1,78 @@
+package dataaccess;
+
+import model.AuthData;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQLAuthDAOTests {
+
+    SQLAuthDAO authDAO;
+
+    @BeforeEach
+    void setup() throws DataAccessException {
+        authDAO = new SQLAuthDAO();
+        authDAO.clear();
+    }
+
+    @Test
+    public void createAuthPositive() throws DataAccessException {
+        AuthData auth = new AuthData("token1", "user1");
+        authDAO.createAuth(auth);
+        AuthData result = authDAO.getAuth("token1");
+        assertNotNull(result);
+        assertEquals("token1", result.authToken());
+        assertEquals("user1", result.username());
+    }
+
+    @Test
+    public void createAuthNegative() {
+        assertThrows(DataAccessException.class, () -> authDAO.createAuth(null));
+    }
+
+    @Test
+    public void getAuthPositive() throws DataAccessException {
+        AuthData auth = new AuthData("token1", "user1");
+        authDAO.createAuth(auth);
+        AuthData result = authDAO.getAuth("token1");
+        assertNotNull(result);
+        assertEquals("token1", result.authToken());
+        assertEquals("user1", result.username());
+    }
+
+    @Test
+    public void getAuthNegative() throws DataAccessException {
+        AuthData result = authDAO.getAuth("nonexistentToken");
+        assertNull(result);
+    }
+
+    @Test
+    public void deleteAuthPositive() throws DataAccessException {
+        AuthData auth = new AuthData("tokenDel", "userDel");
+        authDAO.createAuth(auth);
+        authDAO.deleteAuth("tokenDel");
+        AuthData result = authDAO.getAuth("tokenDel");
+        assertNull(result);
+    }
+
+    @Test
+    public void deleteAuthNegative() {
+        assertDoesNotThrow(() -> {
+            authDAO.deleteAuth("nonexistentToken"); // deleting missing token shouldn't crash
+        });
+    }
+
+    @Test
+    public void clearPositive() throws DataAccessException {
+        authDAO.createAuth(new AuthData("t1", "u1"));
+        authDAO.createAuth(new AuthData("t2", "u2"));
+        authDAO.clear();
+        assertNull(authDAO.getAuth("t1"));
+        assertNull(authDAO.getAuth("t2"));
+    }
+
+    @Test
+    public void clearNegative() {
+        assertDoesNotThrow(() -> authDAO.clear());
+    }
+}
