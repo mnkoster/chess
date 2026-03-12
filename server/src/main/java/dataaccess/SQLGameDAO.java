bff9a73d999c74e947d88d6b65333174ba28f9de
mnkoster <144175990+mnkoster@users.noreply.github.com>
2026-03-11 18:54:47 -0600

    impl (p4) - SQLGameDAO implementing, not passing persistence

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,4 +1,160 @@
 package dataaccess;
 
-public class SQLGameDAO {
+import chess.ChessGame;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import model.GameData;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 3/11/26: added for p4 implementation (database)
+ */
+public class SQLGameDAO implements GameDAO {
+
+    private final Gson gson;
+
+    public SQLGameDAO() throws DataAccessException {
+        this.gson = new GsonBuilder().serializeNulls().create();
+        configureDatabase();
+    }
+
+    // Configure the database
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        var createGamesTable = """
+                CREATE TABLE IF NOT EXISTS games(
+                    gameID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                    whiteUsername VARCHAR(255) NULL,
+                    blackUsername VARCHAR(255) NULL,
+                    gameName VARCHAR (255) NOT NULL,
+                    game TEXT NOT NULL
+                )
+                """;
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(createGamesTable)) {
+            statement.executeUpdate();
+        } catch (Exception ex) {
+            throw new DataAccessException("unable to configure database", ex);
+        }
+    }
+
+    // Interface function - added 3/11/26
+    @Override
+    public Collection<GameData> getListGames() throws DataAccessException {
+        var listGames = "SELECT * FROM games";
+        Collection<GameData> gamesList = new ArrayList<>();
+
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(listGames);
+             ResultSet rs = statement.executeQuery()) {
+
+            while (rs.next()) {
+                int gameID = rs.getInt("gameID");
+                String gameName = rs.getString("gameName");
+                String whitePlayer = rs.getString("whiteUsername");
+                String blackPlayer = rs.getString("blackUsername");
+                String gameJson = rs.getString("game");
+                ChessGame game = gson.fromJson(gameJson, ChessGame.class);
+
+                gamesList.add(new GameData(gameID, gameName, whitePlayer, blackPlayer, game));
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException("unable to get list of games", ex);
+        }
+        return gamesList;
+    }
+
+    // Interface function - added 3/11/26
+    @Override
+    public int createGame(String gameName) throws DataAccessException {
+        var createGame = """
+                INSERT INTO games(whiteUsername, blackUsername, gameName, game)
+                VALUES (NULL, NULL, ?, ?)
+                """;
+        try (var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement(createGame, Statement.RETURN_GENERATED_KEYS)) {
+
+            ChessGame newGame = new ChessGame();
+            String gameJson = gson.toJson(newGame);
+
+            statement.setString(1, gameName);
+            statement.setString(2, gameJson);
+            statement.executeUpdate();
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
+                if (keys.next()) {
+                    return keys.getInt(1);
+                } else {
+                    throw new DataAccessException("unable to get generated gameID");
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException("unable to create game", ex);
+        }
+    }
+
+    // Interface function - added 3/11/26
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        var getGame = """
+                SELECT gameID, whiteUsername, blackUsername, gameName, game
+                FROM games
+                WHERE gameID = ?
+                """;
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(getGame)) {
+            statement.setInt(1, gameID);
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    String gameName = rs.getString("gameName");
+                    String white = rs.getString("whiteUsername");
+                    String black = rs.getString("blackUsername");
+                    String gameJson = rs.getString("game");
+                    ChessGame game = gson.fromJson(gameJson, ChessGame.class);
+
+                    return new GameData(gameID, white, black, gameName, game);
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException("unable to get game", ex);
+        }
+        return null;
+    }
+
+    // Interface function - added 3/11/26
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        String updateGame = """
+            UPDATE games SET whiteUsername = ?, blackUsername = ?, gameName = ?, game = ?
+            WHERE gameID = ?
+            """;
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(updateGame)) {
+
+            statement.setString(1, game.whiteUsername());
+            statement.setString(2, game.blackUsername());
+            statement.setString(3, game.gameName());
+            statement.setString(4, gson.toJson(game.game()));
+            statement.setInt(5, game.gameID());
+            statement.executeUpdate();
+        } catch (Exception ex) {
+            throw new DataAccessException("unable to update game", ex);
+        }
+    }
+
+    // Interface function - added 3/11/26
+    @Override
+    public void clear() throws DataAccessException {
+        var clearGames = "DELETE FROM games";
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(clearGames)) {
+            statement.executeUpdate();
+        } catch (Exception ex) {
+            throw new DataAccessException("unable to clear games", ex);
+        }
+    }
 }
