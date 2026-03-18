f8af38cb4667931d86f62b5195fcfcb15b866d61
mnkoster <144175990+mnkoster@users.noreply.github.com>
2026-03-17 20:04:13 -0600

    impl (p5) - set-up

---
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -0,0 +1,40 @@
+package client;
+
+import org.junit.jupiter.api.*;
+import server.Server;
+
+/**
+ * 3/17/26: added for p5 pre-gameplay
+ */
+public class ServerFacadeTests {
+
+    private static Server server;
+    static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(0);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+    @BeforeEach
+    static void clearDatabase() {
+        database.clear();
+    }
+
+    /**
+     * ADD ONE POSITIVE AND NEGATIVE PER FUNCTION IN SERVERFACADE
+     */
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,7 @@
+package server;
+
+/**
+ * 3/17/26: added for p5 pre-gameplay
+ */
+public class ServerFacade {
+}
